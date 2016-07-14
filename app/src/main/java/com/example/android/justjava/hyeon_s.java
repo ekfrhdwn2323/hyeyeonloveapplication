042e811ad7446968e9b056b06acc8faec7058a1e
ekfrhdwn2323 <ekfrhdwn2323@naver.com>
2016-07-14 11:37:01 +0800

    updated

--- app/src/main/java/com/example/android/justjava/hyeon_s.java
+++ app/src/main/java/com/example/android/justjava/hyeon_s.java
@@ -2,6 +2,8 @@ package com.example.android.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class hyeon_s extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class hyeon_s extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hyeon_s);
+    }
+
+    public void submitORDER(View view)
+    {
+        display(1);
+    }
+
+    private void display (int number)
+    {
+        TextView quantityTextView =(TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(""+number);
     }
 }
